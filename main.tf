a89ac2857c6beed6ee1256f32ece978603439401
Dmytro Kolchak <dima.kolhac@gmail.com>
2026-03-10 02:29:05 +0200

    1

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_virtual_machine" "main" {
   location            = azurerm_resource_group.taskgroup.location
   resource_group_name = azurerm_resource_group.taskgroup.name
   network_interface_ids = [
-    values(azurerm_network_interface.main)[count.index].id
+    azurerm_network_interface.main[local.network_interface_names[count.index]].id
   ]
   vm_size = "Standard_DS1_v2"
 
